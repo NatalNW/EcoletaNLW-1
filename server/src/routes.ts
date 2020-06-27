5ba7458ec4d738ccea5b5038a5e9694b33ee9dcc
NatalNW <natanaelweslley1@gmail.com>
2020-06-27 15:05:34 -0300

    ulter config for upload images

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,18 +1,17 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import PointsController from './controller/PointsController';
 import ItemsController from './controller/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get('/', (req, res) => {
-    return res.json({message:  'Hello Estranho!'});   
-});
-
 routes.get('/items', itemsController.index);
-routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/point/:id', pointsController.show);
+routes.post('/points', upload.single('image'), pointsController.create);
 
 export default routes;
